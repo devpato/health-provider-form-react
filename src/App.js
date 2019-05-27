cebe4bfa98087e71dc50c6358627c6c50dca1776
pevargasg <wearetamo@gmail.com>
2019-05-26 19:01:33 -0700

    injected form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './components/form';
 
-function App() {
-	return <div className="App" />;
+class App extends Component {
+	submit = (values) => {
+		window.alert(JSON.stringify(values));
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>New Provider Form</h1>
+				<Form onSubmit={this.submit} initialValues={this.getInitialValues()} />
+			</div>
+		);
+	}
+
+	getInitialValues() {
+		return {
+			fullName: '',
+			npi: '',
+			bussinessAddress: '',
+			telephone: '',
+			email: ''
+		};
+	}
 }
 
 export default App;
--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { renderInput } from './field';
+
 class Form extends Component {
 	render() {
 		const { handleSubmit } = this.props;
 		console.log(handleSubmit);
 		return (
 			<form onSubmit={handleSubmit} className="ui form error">
-				<Field name="fullName" component={renderInput} type="text" placeholder="Full Name" />
-				<Field name="npi" component={renderInput} type="text" placeholder="NPI" />
+				<Field name="fullName" component="input" type="text" placeholder="Full Name" />
+				<Field name="npi" component="input" type="text" placeholder="NPI" />
 				<button type="submit" label="submit" className="ui button primary">
 					Create
 				</button>
