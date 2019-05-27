64c58d641e78d1217ae18ac3446e2dcf9bc8e36a
pevargasg <wearetamo@gmail.com>
2019-05-26 18:54:54 -0700

    form component

--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-
-const roorReducer = combineReducers({ form: formReducer });
