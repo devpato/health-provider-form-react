3f2fff2e745d4fe276026b06392bdc9d2fa7131c
Patricio <wearetamo@gmail.com>
2019-05-26 21:37:31 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/form';
+import logo from './assets/availity-logo.png';
+
 class App extends Component {
-	submit = (values) => {
-		console.log(JSON.stringify(values));
-	};
+  submit = values => {
+    console.log(JSON.stringify(values));
+  };
 
-	render() {
-		return (
-			<div className="registration-form ui container">
-				<img src="../../assets/availity-logo.png" />
-				<h1>New Provider Form</h1>
-				<Form onSubmit={this.submit} initialValues={this.getInitialValues()} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='registration-form ui container'>
+        <img src={logo} />
+        <h1>New Provider Form</h1>
+        <Form onSubmit={this.submit} initialValues={this.getInitialValues()} />
+      </div>
+    );
+  }
 
-	getInitialValues() {
-		return {
-			fullName: '',
-			npi: '',
-			businessAddress: '',
-			telephone: '',
-			email: ''
-		};
-	}
+  getInitialValues() {
+    return {
+      fullName: '',
+      npi: '',
+      businessAddress: '',
+      telephone: '',
+      email: ''
+    };
+  }
 }
 
 export default App;
