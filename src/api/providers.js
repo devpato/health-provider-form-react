ac71314a280afb3fd67bf237115b00fe8397b742
Patricio <wearetamo@gmail.com>
2019-05-28 00:57:09 -0400

    added the end point

--- src/api/providers.js
+++ src/api/providers.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL:
-    'https://p2pqluc1s5.execute-api.us-east-1.amazonaws.com/providers-api/providers'
+    'https://p2pqluc1s5.execute-api.us-east-1.amazonaws.com/providers-api'
 });
